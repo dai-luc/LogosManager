317ffc563d4f99b3a037724c519cfc6e401bf8c3
Dai <daisuke.92.r.k@gmail.com>
2020-02-24 20:46:03 +0900

    git公開用2

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/LogosManager/favicon.ico' }/* href: '/favicon.ico' */
+      { rel: 'icon', type: 'image/x-icon', href: '/LogosManager/dist/favicon.ico' }/* href: '/favicon.ico' */
     ]
   },
   /*
@@ -60,7 +60,7 @@ export default {
     }
   },
   router: {
-    base: '/LogosManager/'
+    base: '/LogosManager/dist/'
   } /* 本番はbase: '/' */
 
 }
